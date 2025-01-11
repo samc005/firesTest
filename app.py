9ca465563ee2eb13c6424ed4d5c92a9bc851148f
Samprita C <sampritac9@gmail.com>
2025-01-11 15:31:32 -0800

    t18

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Add a Marker on Click (or any other interactive layer like GeoJSON)
-st.markdown("<h1 style='text-align: center; color:white;'>Click on the map to select a location:</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color:white;'>Click on the map to select a location:</h2>", unsafe_allow_html=True)
 
 st.markdown("<p style='text-align: center; color: #FF5733;'>This model predicts the likelihood of wildfire occurrence based on environmental factors. Click on the map to choose a location, and the model will fetch environmental data for prediction.</p>", unsafe_allow_html=True)
 
