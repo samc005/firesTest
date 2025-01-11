d388d3b2cfdf86ae6da8bae1917ebf71b7fd75e3
Samprita C <sampritac9@gmail.com>
2025-01-11 12:05:40 -0800

    18

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ from joblib import load
 
 import streamlit as st
 
-# Center the title using HTML
-st.markdown("<h1 style='text-align: center; color: rgb(227, 92, 55);'>Wildfire Prediction Model</h1>", unsafe_allow_html=True)
-
-
-
 def get_input():
     elevation = st.number_input("Elevation (m): ")
     population = st.number_input("Population: ")
