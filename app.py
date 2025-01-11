9764dfa8eb5c673a549cfad4832e727200ef6fec
natashaj13 <73080708+natashaj13@users.noreply.github.com>
2025-01-11 09:12:20 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+from joblib import load
+
+#model = load("model.joblib")
+
+def get_input():
+    elevation = st.number_input("Elevation (m): ")
+    population = st.number_input("Population: ")
+    input_features = [[elevation, population]]
+    return input_features
+
+def predict(model, input):
+    return model.predict(input)
+
+def get_prediction():
+
+
+input_features = get_input()
+prediction = predict(model, input_features)
+#get_prediction(prediction)
+st.write("Results")
