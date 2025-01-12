701bcb00deeaa5c1a8ff9e4e541763f82c7eaccc
samc005 <113663437+samc005@users.noreply.github.com>
2025-01-12 02:26:52 -0800

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ if tab == "Home":
     # Click
     st.markdown("""
         <h1 style='color: #FF5733; font-family: Tahoma; font-size: 50px;'>FireWatch</h1>
-        <p style='color: #E28743; font-family: Tahoma; font-size: 20px; text-align: center; padding-right: 20px;'>Real-Time Wildfire Predictor</p>
+        <p style='color: #E28743; font-family: Tahoma; font-size: 20px; text-align: center; padding-right: 20px;'>A Real-Time Wildfire Predictor</p>
     """, unsafe_allow_html=True)
     st.write('')
     st.write('')
