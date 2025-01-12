817f905bb314cedd625ee4b04beb50721528fcf6
samc005 <113663437+samc005@users.noreply.github.com>
2025-01-12 02:45:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,15 @@ import datetime
 from getData import get_weather_data
 from getData import get_fire_data
 
+
+from langchain import OpenAI
+from langchain.callbacks import get_openai_callback
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationSummaryMemory
+from langchain.memory import ConversationSummaryMemory
+import streamlit.components.v1 as components
+
+
 st.set_page_config(
     page_title="FireWatch",  # Title that will appear in the browser tab
     page_icon="🔥",          # Icon that will appear in the browser tab (can use text or image)
@@ -195,6 +204,7 @@ st.markdown("""
     <div class="navbar">
         <a href="?page=Home" class="{% if page == 'Home' %}active{% endif %}">Home</a>
         <a href="?page=resources" class="{% if page == 'resources' %}active{% endif %}">Resources</a>
+        <a href="?page=chatbot" class="{% if page == 'chatbot' %}active{% endif %}">Chat Bot</a>
     </div>
 """, unsafe_allow_html=True)
 
@@ -304,3 +314,114 @@ elif tab == "resources":
         <p><a href='https://readyforwildfire.org/post-wildfire/who-can-help' target='_blank'>Organizations that can help with wildfire recovery</a></p>
     </div>
     """, unsafe_allow_html=True)
+
+elif tab == "chatbot":
+    @dataclass
+    class Message: #keeps track of messages
+        origin: Literal["human", "AI"]
+        message: str
+
+    def load_css():
+        with open("static/styles.css", "r") as f:
+            css = f"<style>{f.read()}</style>"
+            st.markdown(css, unsafe_allow_html=True)
+
+    def init_state():
+        if "history" not in st.session_state:
+            st.session_state.history = []
+        if "token_count" not in st.session_state:
+            st.session_state.token_count = 0
+        if "conversation" not in st.session_state:
+            llm = OpenAI(temperature=0, openai_api_key=st.secrets["openai”][openai_api_key"], modName="text-davinci-003")
+            st.session_state.conversation = ConversationChain(
+                llm = llm,
+                memory = ConversationSummaryMemory(llm=llm)
+            )
+
+    def on_click_callback():
+        with get_openai_callback() as cb:
+            prompt = st.session_state.prompt
+            response = st.session_state.conversation.run(prompt)
+            st.session_state.history.append(
+                Message("human", prompt)
+            )
+            st.session_state.history.append(
+                Message("AI", response)
+            )
+            st.session_state.token_count += cb.total_tokens
+
+    load_css()
+    init_state()
+
+    st.markdown("<h1 style='text-align: center; color: #FF5733; font-family: Georgia; font-size: 50px;'>TALK TO US</h1>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; '>Ask any questions about wildfires and get predictions and safety tips!</p>", unsafe_allow_html=True)
+
+    chat_hold = st.container()
+    prompt_hold = st.form("chat-form")
+    credit_hold = st.empty()
+
+    with chat_hold:
+        for chat in st.session_state.history:
+            div = f"""
+    <div class="chat-row 
+        {'' if chat.origin == 'AI' else 'row-reverse'}">
+        <img class="chat-icon" src="app/static/{
+            'ai_icon.png' if chat.origin == 'AI' 
+                          else 'user_icon.png'}"
+             width=32 height=32>
+        <div class="chat-bubble
+        {'ai-bubble' if chat.origin == 'AI' else 'human-bubble'}">
+            &#8203;{chat.message}
+        </div>
+    </div>
+            """
+            st.markdown(div, unsafe_allow_html=True)
+        
+        for _ in range(3):
+            st.markdown("")
+    
+    with prompt_hold:
+        st.markdown("**Chat**")
+        cols = st.columns((6, 1))
+        cols[0].text_input(
+            "Chat",
+            value="Hello!",
+            label_visibility="collapsed",
+            key="prompt",
+        )
+        cols[1].form_submit_button(
+            "Submit", 
+            type="primary", 
+            on_click=on_click_callback, 
+        )
+    
+    credit_hold.caption(f"""
+    Used {st.session_state.token_count} tokens \n
+    Debug Langchain conversation: 
+    {st.session_state.conversation.memory.buffer}
+    """)
+
+    components.html("""
+    <script>
+    const streamlitDoc = window.parent.document;
+
+    const buttons = Array.from(
+        streamlitDoc.querySelectorAll('.stButton > button')
+    );
+    const submitButton = buttons.find(
+        el => el.innerText === 'Submit'
+    );
+    
+    streamlitDoc.addEventListener('keydown', function(e) {
+        switch (e.key) {
+            case 'Enter':
+                submitButton.click();
+                break;
+        }
+    });
+    </script>
+    """, 
+        height=0,
+        width=0,
+    )
+
